d1f145ce7b9e666321edc15e252fb3119c55bb9b
Oliver Kowalke <oliver.kowalke@gmail.com>
2012-03-21 07:52:00 +0000

    context: add support for 32bit MacOS X
    [SVN r77449]

--- example/pic.sh
+++ example/pic.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-g++ -x assembler-with-cpp -fPIC -O3 -finline-functions -Wno-inline -Wall -march=native -DBOOST_ALL_NO_LIB=1 -DBOOST_CONTEXT_DYN_LINK=1 -DNDEBUG -I../../.. -c -o fcontext_x86_64_sysv_elf_gas.o ../src/asm/fcontext_x86_64_sysv_elf_gas.S 
+g++ -x assembler-with-cpp -fPIC -O3 -finline-functions -Wno-inline -Wall -march=native -DBOOST_ALL_NO_LIB=1 -DBOOST_CONTEXT_DYN_LINK=1 -DNDEBUG -I../../.. -c -o fcontext_i386_sysv_elf_gas.o ../src/asm/fcontext_i386_sysv_elf_gas.S 
 
 g++ -ftemplate-depth-128 -fPIC -O3 -finline-functions -Wno-inline -Wall -march=native -DBOOST_ALL_NO_LIB=1 -DBOOST_CONTEXT_DYN_LINK=1 -DNDEBUG -I../../.. -c -o fcontext.o ../src/fcontext.cpp 
 
@@ -8,6 +8,6 @@ g++ -ftemplate-depth-128 -fPIC -O3 -finline-functions -Wno-inline -Wall -march=n
 
 g++ -ftemplate-depth-128 -fPIC -O3 -finline-functions -Wno-inline -Wall -march=native -DBOOST_ALL_NO_LIB=1 -DBOOST_CONTEXT_DYN_LINK=1 -DNDEBUG -I../../.. -c -o stack_utils_posix.o ../src/stack_utils_posix.cpp
 
-gcc -shared -Wl,-soname,libboost_my_lib.so -o libboost_my_lib.so fcontext.o stack_allocator_posix.o stack_utils_posix.o fcontext_x86_64_sysv_elf_gas.o -lc -lstdc++
+gcc -shared -Wl,-soname,libboost_my_lib.so -o libboost_my_lib.so fcontext.o stack_allocator_posix.o stack_utils_posix.o fcontext_i386_sysv_elf_gas.o -lc -lstdc++
 
 gcc -g ./transfer.cpp -I../../../ -L./ -Wl,-rpath=./ -lboost_my_lib -lrt 
