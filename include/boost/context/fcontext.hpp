6d6affc5945a900be44f2656f8886428bff14b89
Oliver Kowalke <oliver.kowalke@gmail.com>
2013-01-21 19:24:01 +0000

    context: support for architecture SPARC added
    [SVN r82583]

--- include/boost/context/fcontext.hpp
+++ include/boost/context/fcontext.hpp
@@ -59,6 +59,9 @@ typedef int intptr_t;
     || defined(__ppc__) || defined(_ARCH_PPC) || defined(__POWERPC__) \
     || defined(__PPCGECKO__) || defined(__PPCBROADWAY) || defined(_XENON)
 # include <boost/context/detail/fcontext_ppc.hpp>
+#elif defined(__sparc__) || defined(__sparc)
+// sparc or sparc64
+# include <boost/context/detail/fcontext_sparc.hpp>
 #else
 # error "platform not supported"
 #endif
