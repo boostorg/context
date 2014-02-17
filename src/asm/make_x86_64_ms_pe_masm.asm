b31c44c5b37095959c2f75b7d25c640905c6c537
Oliver Kowalke <oliver.kowalke@gmx.de>
2014-02-17 22:17:28 +0100

    makefcontext() for 64bit Windows

--- src/asm/make_x86_64_ms_pe_masm.asm
+++ src/asm/make_x86_64_ms_pe_masm.asm
@@ -9,42 +9,36 @@
 ;  ----------------------------------------------------------------------------------
 ;  |   0x0   |   0x4   |   0x8   |   0xc   |   0x10   |   0x14  |   0x18  |   0x1c  |
 ;  ----------------------------------------------------------------------------------
-;  |        R12        |         R13       |         R14        |        R15        |
+;  | fc_mxcsr|fc_x87_cw|      fbr_strg     |      fc_dealloc    |       limit       |
 ;  ----------------------------------------------------------------------------------
 ;  ----------------------------------------------------------------------------------
 ;  |    8    |    9    |   10    |   11    |    12    |    13   |    14   |    15   |
 ;  ----------------------------------------------------------------------------------
 ;  |   0x20  |   0x24  |   0x28  |  0x2c   |   0x30   |   0x34  |   0x38  |   0x3c  |
 ;  ----------------------------------------------------------------------------------
-;  |        RDI        |        RSI        |         RBX        |        RBP        |
+;  |       base        |       R12         |         R13        |         R14       |
 ;  ----------------------------------------------------------------------------------
 ;  ----------------------------------------------------------------------------------
-;  |    16   |    17   |    18   |    19   |                                        |
+;  |    16   |    17   |    18   |    19   |    20   |    21    |    22   |    23   |
 ;  ----------------------------------------------------------------------------------
-;  |   0x40  |   0x44  |   0x48  |   0x4c  |                                        |
+;  |   0x40  |   0x44  |   0x48  |   0x4c  |   0x50  |   0x54   |   0x58  |   0x5c  |
 ;  ----------------------------------------------------------------------------------
-;  |          |        RIP        |                                        |
-;  ----------------------------------------------------------------------------------
-;  ----------------------------------------------------------------------------------
-;  |    20   |    21   |    22   |    23   |    24    |    25   |                   |
-;  ----------------------------------------------------------------------------------
-;  |   0x50  |   0x54  |   0x58  |   0x5c  |   0x60   |   0x64  |                   |
-;  ----------------------------------------------------------------------------------
-;  |        sp         |       size        |        limit       |                   |
+;  |        R15        |        RDI        |        RSI         |         RBX       |
 ;  ----------------------------------------------------------------------------------
 ;  ----------------------------------------------------------------------------------
-;  |    26   |   27    |                                                            |
+;  |    24   |    25   |    26   |   27    |    28    |   29    |                   |
 ;  ----------------------------------------------------------------------------------
-;  |   0x68  |   0x6c  |                                                            |
+;  |  0x60   |   0x64  |   0x68  |   0x6c  |   0x70   |   0x74  |                   |
 ;  ----------------------------------------------------------------------------------
-;  |      fbr_strg     |                                                            |
+;  |        RBP        |        RIP        |       EXIT         |                   |
 ;  ----------------------------------------------------------------------------------
+
 ;  ----------------------------------------------------------------------------------
 ;  |    28   |   29    |    30   |    31   |    32    |    33   |   34    |   35    |
 ;  ----------------------------------------------------------------------------------
 ;  |   0x70  |   0x74  |   0x78  |   0x7c  |   0x80   |   0x84  |  0x88   |  0x8c   |
 ;  ----------------------------------------------------------------------------------
-;  | fc_mxcsr|fc_x87_cw|      fc_xmm       |      SEE registers (XMM6-XMM15)        |
+;  |     SEE registers (XMM6-XMM15)        |
 ;  ----------------------------------------------------------------------------------
 ;  ----------------------------------------------------------------------------------
 ;  |   36    |    37   |    38   |    39   |    40    |    41   |   42    |   43    |
@@ -81,51 +75,62 @@
 ;  ----------------------------------------------------------------------------------
 ;  |                          SEE registers (XMM6-XMM15)                            |
 ;  ----------------------------------------------------------------------------------
-;  ----------------------------------------------------------------------------------
-;  |    76   |   77    |                                                            |
-;  ----------------------------------------------------------------------------------
-;  |  0x130  |  0x134  |                                                            |
-;  ----------------------------------------------------------------------------------
-;  |     fc_dealloc    |                                                            |
-;  ----------------------------------------------------------------------------------
 
-EXTERN  _exit:PROC            ; standard C library function
+; standard C library function
+EXTERN  _exit:PROC
 .code
 
-make_fcontext PROC EXPORT FRAME  ; generate function table entry in .pdata and unwind information in
-    .endprolog                   ; .xdata for a function's structured exception handling unwind behavior
+; generate function table entry in .pdata and unwind information in
+make_fcontext PROC EXPORT FRAME
+    ; .xdata for a function's structured exception handling unwind behavior
+    .endprolog
 
-    lea  rax,        [rcx-0138h] ; reserve space for fcontext_t at top of context stack
+    ; first arg of make_fcontext() == top of context-stack
+    mov  rax, rcx
 
     ; shift address in RAX to lower 16 byte boundary
     ; == pointer to fcontext_t and address of context stack
-    and  rax,        -16
+    and  rax, -16
 
-    mov  [rax+048h], r8          ; save address of context function in fcontext_t
-    mov  [rax+058h], rdx         ; save context stack size in fcontext_t
-    mov  [rax+050h], rcx         ; save address of context stack pointer (base) in fcontext_t
+    ; reserve space for context-data on context-stack
+    ; size for fc_mxcsr .. RIP + return-address for context-function
+    ; on context-function entry: (RSP -0x8) % 16 == 0
+    lea  rax, [rax-078h]
 
-    neg  rdx                     ; negate stack size for LEA instruction (== substraction)
-    lea  rcx,         [rcx+rdx]  ; compute bottom address of context stack (limit)
-    mov  [rax+060h],  rcx        ; save bottom address of context stack (limit) in fcontext_t
-    mov  [rax+0130h], rcx        ; save address of context stack limit as 'dealloction stack'
+    ; third arg of make_fcontext() == address of context-function
+    mov  [rax+068h], r8
 
-    stmxcsr [rax+070h]           ; save MMX control and status word
-    fnstcw  [rax+074h]           ; save x87 control word
+    ; first arg of make_fcontext() == top of context-stack
+    ; save top address of context stack as 'base'
+    mov  [rax+020h], rcx
+    ; second arg of make_fcontext() == size of context-stack
+    ; negate stack size for LEA instruction (== substraction)
+    neg  rdx
+    ; compute bottom address of context stack (limit)
+    lea  rcx, [rcx+rdx]
+    ; save bottom address of context stack as 'limit'
+    mov  [rax+018h], rcx
+    ; save address of context stack limit as 'dealloction stack'
+    mov  [rax+010h], rcx
 
-    lea  rdx,        [rax-028h]  ; reserve 32byte shadow space + return address on stack, (RSP - 0x8) % 16 == 0
-    mov  [rax+040h], rdx         ; save address in RDX as stack pointer for context function
+    ; save MMX control- and status-word
+    stmxcsr  [rax]
+    ; save x87 control-word
+    fnstcw  [rax+04h]
 
-    lea  rcx,        finish      ; compute abs address of label finish
-    mov  [rdx],      rcx         ; save address of finish as return address for context function
-                                 ; entered after context function returns
+    ; compute abs address of label finish
+    lea  rcx, finish
+    ; save address of finish as return-address for context-function
+    ; will be entered after context-function returns
+    mov  [rax+070h], rcx
 
-    ret
+    ret ; return pointer to context-data
 
 finish:
-    ; RSP points to same address as RSP on entry of context function + 0x8
-    xor   rcx,       rcx         ; exit code is zero
-    call  _exit                  ; exit application
+    ; exit code is zero
+    xor  rcx, rcx
+    ; exit application
+    call  _exit
     hlt
 make_fcontext ENDP
 END
