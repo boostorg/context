a7be53c71e6252d8ff20114100a11cfe1e507029
Oliver Kowalke <oliver.kowalke@gmx.de>
2015-08-21 08:47:57 +0200

    fix #11565 - segmented stacks not build for gcc-5.2

--- include/boost/context/detail/config.hpp
+++ include/boost/context/detail/config.hpp
@@ -50,8 +50,8 @@
 #endif
 
 #if defined(BOOST_USE_SEGMENTED_STACKS)
-# if ! ( (defined(__GNUC__) && __GNUC__ > 3 && __GNUC_MINOR__ > 6) || \
-         (defined(__clang__) && __clang_major__ > 2 && __clang_minor__ > 3) )
+# if ! ( (defined(__GNUC__) && (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 6) ) ) || \
+         (defined(__clang__) && (__clang_major__ > 2 || ( __clang_major__ == 2 && __clang_minor__ > 3) ) ) )
 #  error "compiler does not support segmented_stack stacks"
 # endif
 # define BOOST_CONTEXT_SEGMENTS 10
