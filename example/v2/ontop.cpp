1a72352c9c741bfcf43f5086c3d9d1d0f1cff52b
Oliver Kowalke <oliver.kowalke@gmx.de>
2016-02-10 20:59:59 +0100

    update ontop example

--- example/v2/ontop.cpp
+++ example/v2/ontop.cpp
@@ -14,9 +14,9 @@ namespace ctx = boost::context;
 
 ctx::execution_context< int > f1( ctx::execution_context< int > ctx, int data) {
     std::cout << "f1: entered first time: " << data  << std::endl;
-    std::tie( ctx, data) = ctx( data);
+    std::tie( ctx, data) = ctx( data + 1);
     std::cout << "f1: entered second time: " << data  << std::endl;
-    std::tie( ctx, data) = ctx( data);
+    std::tie( ctx, data) = ctx( data + 1);
     std::cout << "f1: entered third time: " << data << std::endl;
     return ctx;
 }
@@ -27,13 +27,13 @@ std::tuple< ctx::execution_context< int >, int > f2( ctx::execution_context< int
 }
 
 int main() {
-    int data = 3;
+    int data = 0;
     ctx::execution_context< int > ctx( f1);
-    std::tie( ctx, data) = ctx( data);
+    std::tie( ctx, data) = ctx( data + 1);
     std::cout << "f1: returned first time: " << data << std::endl;
-    std::tie( ctx, data) = ctx( data);
+    std::tie( ctx, data) = ctx( data + 1);
     std::cout << "f1: returned second time: " << data << std::endl;
-    std::tie( ctx, data) = ctx( ctx::exec_ontop_arg, f2, data + 2);
+    std::tie( ctx, data) = ctx( ctx::exec_ontop_arg, f2, data + 1);
     std::cout << "f1: returned third time" << std::endl;
 
     std::cout << "main: done" << std::endl;
