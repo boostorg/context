50ebf5bd1160623cbbf169f6a5d7ddff04e9d1ef
Oliver Kowalke <oliver.kowalke@gmail.com>
2017-04-24 17:37:29 +0200

    x64 Windows: store/load fiber local storage from correct offset

--- src/asm/ontop_x86_64_ms_pe_masm.asm
+++ src/asm/ontop_x86_64_ms_pe_masm.asm
@@ -111,7 +111,7 @@ ENDIF
     ; load NT_TIB
     mov  r10,  gs:[030h]
     ; save fiber local storage
-    mov  rax, [r10+018h]
+    mov  rax, [r10+020h]
     mov  [rsp+0b0h], rax
     ; save current deallocation stack
     mov  rax, [r10+01478h]
@@ -162,7 +162,7 @@ ENDIF
     mov  r10,  gs:[030h]
     ; restore fiber local storage
     mov  rax, [rsp+0b0h]
-    mov  [r10+018h], rax
+    mov  [r10+020h], rax
     ; restore current deallocation stack
     mov  rax, [rsp+0b8h]
     mov  [r10+01478h], rax
