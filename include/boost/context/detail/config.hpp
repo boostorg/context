7254c771108ff3f7729478d9efc8b04f8b87e1ed
Oliver Kowalke <oliver.kowalke@gmail.com>
2017-06-04 12:47:28 +0200

    add prefetching

--- include/boost/context/detail/config.hpp
+++ include/boost/context/detail/config.hpp
@@ -104,4 +104,13 @@
 # endif
 #endif
 
+// modern architectures have cachelines with 64byte length
+// ARM Cortex-A15 32/64byte, Cortex-A9 16/32/64bytes
+// MIPS 74K: 32byte, 4KEc: 16byte
+// ist should be safe to use 64byte for all
+static constexpr std::size_t cache_alignment{ 64 };
+static constexpr std::size_t cacheline_length{ 64 };
+// lookahead size for prefetching
+static constexpr std::size_t prefetch_stride{ 4 * cacheline_length };
+
 #endif // BOOST_CONTEXT_DETAIL_CONFIG_H
