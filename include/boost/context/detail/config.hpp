f62fbbe3189bde4e88e7e1f10714cc03a67589e1
Oliver Kowalke <oliver.kowalke@gmail.com>
2019-03-14 15:00:19 +0100

    test for CXXABI

--- include/boost/context/detail/config.hpp
+++ include/boost/context/detail/config.hpp
@@ -115,4 +115,19 @@ static constexpr std::size_t cacheline_length{ 64 };
 static constexpr std::size_t prefetch_stride{ 4 * cacheline_length };
 #endif
 
+// __has_include is currently supported by GCC and Clang. However GCC 4.9 may have issues and
+// returns 1 for 'defined( __has_include )', while '__has_include' is actually not supported:
+// https://gcc.gnu.org/bugzilla/show_bug.cgi?id=63662
+#if defined(__has_include) && (!defined(BOOST_GCC) || (__GNUC__ + 0) >= 5)
+# if __has_include(<cxxabi.h>)
+#  define BOOST_CONTEXT_HAS_CXXABI_H
+# endif
+#elif defined(__GLIBCXX__) || defined(__GLIBCPP__)
+# define BOOST_CONTEXT_HAS_CXXABI_H
+#endif
+
+#if defined( BOOST_CONTEXT_HAS_CXXABI_H )
+# include <cxxabi.h>
+#endif
+
 #endif // BOOST_CONTEXT_DETAIL_CONFIG_H
