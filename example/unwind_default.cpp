3df6609ac522b5eb7ab906198f59fe14c8bfc0da
Oliver Kowalke <oliver.kowalke@gmail.com>
2019-07-27 14:12:00 +0200

    examples regarding to unwinding

---
+++ example/unwind_default.cpp
@@ -0,0 +1,63 @@
+
+//          Copyright Oliver Kowalke 2019.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+
+#include <boost/context/fiber_context.hpp>
+
+namespace ctx = boost::context;
+
+class fibonacci_generator {
+private:
+    ctx::fiber_context  fcallee;
+    uint64_t            a;
+
+    void run_(ctx::fiber_context && fcaller) {
+        a=0;
+        int b=1;
+        for(;;){
+            fcaller = std::move( fcaller).resume();
+            int next=a+b;
+            a=b;
+            b=next;
+        }
+    }
+
+public:
+    fibonacci_generator() :
+        fcallee{
+            [this](ctx::fiber_context && fcaller) -> ctx::fiber_context {
+                run_( std::move( fcaller) );
+                return {};
+        } } {
+    }
+
+    ~fibonacci_generator() {
+        if ( fcallee) {
+            std::move( fcallee).resume_with( ctx::unwind_fiber);
+        }
+    }
+
+    fibonacci_generator( fibonacci_generator const&) = delete;
+    fibonacci_generator & operator=( fibonacci_generator const&) = delete;
+
+    uint64_t operator()() {
+        fcallee = std::move( fcallee).resume();
+        return a;
+    }
+};
+
+int main() {
+    fibonacci_generator generator;
+    for ( int j = 0; j < 10; ++j) {
+        std::cout << generator() << " ";
+    }
+    std::cout << std::endl;
+    std::cout << "main: done" << std::endl;
+    return EXIT_SUCCESS;
+}
---
+++ example/unwind_throw1.cpp
@@ -0,0 +1,75 @@
+
+//          Copyright Oliver Kowalke 2019.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+
+#include <boost/context/fiber_context.hpp>
+
+namespace ctx = boost::context;
+
+class fibonacci_generator {
+private:
+    struct my_unwind_exception {
+    };
+
+    ctx::fiber_context  fcallee;
+    ctx::fiber_context  fexit;
+    uint64_t            a;
+
+    void run_(ctx::fiber_context && fcaller) {
+        a=0;
+        int b=1;
+        for(;;){
+            fcaller = std::move( fcaller).resume();
+            int next=a+b;
+            a=b;
+            b=next;
+        }
+    }
+
+public:
+    fibonacci_generator() :
+        fcallee{
+            [this](ctx::fiber_context && fcaller) -> ctx::fiber_context {
+                try {
+                    run_( std::move( fcaller) );
+                } catch ( my_unwind_exception const&) {
+                }
+                return std::move( fexit);
+        } } {
+    }
+
+    ~fibonacci_generator() {
+        if ( fcallee) {
+            std::move( fcallee).resume_with(
+                    [this](ctx::fiber_context && f) -> ctx::fiber_context {
+                        fexit = std::move( f);
+                        throw my_unwind_exception{};
+                        return {};
+                    });
+        }
+    }
+
+    fibonacci_generator( fibonacci_generator const&) = delete;
+    fibonacci_generator & operator=( fibonacci_generator const&) = delete;
+
+    uint64_t operator()() {
+        fcallee = std::move( fcallee).resume();
+        return a;
+    }
+};
+
+int main() {
+    fibonacci_generator generator;
+    for ( int j = 0; j < 10; ++j) {
+        std::cout << generator() << " ";
+    }
+    std::cout << std::endl;
+    std::cout << "main: done" << std::endl;
+    return EXIT_SUCCESS;
+}
