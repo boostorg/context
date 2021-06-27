eb7c04713220ed9d3b56d3ea7a5d562a8942568b
Oliver Kowalke <oliver.kowalke@gmail.com>
2021-06-27 09:31:11 +0200

    FreeBSD: type of stack_t::ss_sp is `char*`
    FreeBSD defines stack_t::ss_sp as `char*`.

--- include/boost/context/fiber_ucontext.hpp
+++ include/boost/context/fiber_ucontext.hpp
@@ -32,6 +32,7 @@ extern "C" {
 
 #include <boost/assert.hpp>
 #include <boost/config.hpp>
+#include <boost/predef.h>
 
 #include <boost/context/detail/disable_overload.hpp>
 #if defined(BOOST_NO_CXX14_STD_EXCHANGE)
@@ -329,7 +330,12 @@ static fiber_activation_record * create_fiber1( StackAlloc && salloc, Fn && fn) 
                 std::error_code( errno, std::system_category() ),
                 "getcontext() failed");
     }
+#if BOOST_OS_BSD_FREE
+    // because FreeBSD defines stack_t::ss_sp as char *
+    record->uctx.uc_stack.ss_sp = static_cast< char * >( stack_bottom);
+#else
     record->uctx.uc_stack.ss_sp = stack_bottom;
+#endif
     // 64byte gap between control structure and stack top
     record->uctx.uc_stack.ss_size = reinterpret_cast< uintptr_t >( storage) -
             reinterpret_cast< uintptr_t >( stack_bottom) - static_cast< uintptr_t >( 64);
@@ -367,7 +373,12 @@ static fiber_activation_record * create_fiber2( preallocated palloc, StackAlloc 
                 std::error_code( errno, std::system_category() ),
                 "getcontext() failed");
     }
+#if BOOST_OS_BSD_FREE
+    // because FreeBSD defines stack_t::ss_sp as char *
+    record->uctx.uc_stack.ss_sp = static_cast< char * >( stack_bottom);
+#else
     record->uctx.uc_stack.ss_sp = stack_bottom;
+#endif
     // 64byte gap between control structure and stack top
     record->uctx.uc_stack.ss_size = reinterpret_cast< uintptr_t >( storage) -
             reinterpret_cast< uintptr_t >( stack_bottom) - static_cast< uintptr_t >( 64);
@@ -513,7 +524,7 @@ public:
     }
 
     #if !defined(BOOST_EMBTC)
-    
+
     template< typename charT, class traitsT >
     friend std::basic_ostream< charT, traitsT > &
     operator<<( std::basic_ostream< charT, traitsT > & os, fiber const& other) {
@@ -525,7 +536,7 @@ public:
     }
 
     #else
-    
+
     template< typename charT, class traitsT >
     friend std::basic_ostream< charT, traitsT > &
     operator<<( std::basic_ostream< charT, traitsT > & os, fiber const& other);
@@ -550,7 +561,7 @@ public:
     }
 
 #endif
-    
+
 inline
 void swap( fiber & l, fiber & r) noexcept {
     l.swap( r);
